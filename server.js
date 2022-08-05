a876e49e6ceb29a329540c35cc51f415600c7367
Max Wofford <max@maxwofford.com>
2022-08-05 07:05:25 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const fileUpload = require('express-fileupload')
+const app = express()
+
+app.use(express.static('public'))
+
+app.use(fileUpload({
+  createParentPath: true,
+  useTempFiles: true,
+  // limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
+  // limits: { fileSize: 50 * 1024 }, // 50KB
+}))
+
+app.post('/upload', function(req, res) {
+
+  let file = req.files?.file
+  if (!file) {
+    return res.status(400).send('No files were uploaded.')
+  }
+
+  let md5 = file.md5
+  let uploadPath = `/storage/${md5}/${file.name}`
+  file.mv(__dirname + uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err)
+    }
+
+    res.send(uploadPath)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('running server...')
+})
